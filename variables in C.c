5e60ce5afa76ee010d9c4372a306cee682b511dd
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-11-20 09:26:43 +0530

    Add files via upload

---
+++ variables in C.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main()
+{
+  int a=4;
+  float b=8.5;
+  char c='u';
+  
+  int d=45;
+  int e=45+4;
+  //Using "\n" for the Output to Come in one bye one line
+  printf("The Value of a is %d \n",a);
+  printf("The Value of b is %f \n",b);
+  printf("The Value of c is %c \n",c);
+  //Some extra tip regarding additiona,etc..
+  printf("Sum of a and d %d \n",a+d);
+  printf("Sum of a and d %d \n",e);
+    return 0;
+}
+ 
+
+
