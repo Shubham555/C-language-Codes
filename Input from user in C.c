82a9a0dd5ca09be54a0c93fee9f4a17af118ddc1
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-11-21 09:46:16 +0530

    Add files via upload

---
+++ Input from user in C.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+// Receiving input from user and giving to Variable by using "scanf function"
+// Hya a,b,c,.. ya variables khi value dileli naste...
+#include <stdio.h>
+
+int main()
+{
+  int a,b;
+  printf("Enter the value of a\n");
+  scanf("%d",&a);
+  printf("Enter the value of b\n");
+  scanf("%d",&b);
+  printf("Sum of a and b is %d",a+b);
+
+    return 0;
+}
+
+
