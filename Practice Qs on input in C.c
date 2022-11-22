12e5f3835fac9e6a2cf6988a2c10c83bb1651c31
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-11-22 15:50:06 +0530

    Add files via upload

---
+++ Practice Qs on input in C.c
@@ -0,0 +1,18 @@
+//Area of rectangle 1St by Taking inputs...
+
+#include <stdio.h>
+
+int main()
+{ 
+    int Length=3 ,Breadth=8;
+    int   Area=Length*Breadth;
+    
+    printf("Area of Rectangle is %d",Area);
+    return 0;
+}
+
+
+
+
+
+
