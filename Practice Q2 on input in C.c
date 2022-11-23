a48cbfcebd8f178637ee5d0ab89370210f9474a3
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-11-23 11:36:54 +0530

    Add files via upload

---
+++ Practice Q2 on input in C.c
@@ -0,0 +1,17 @@
+//Area of Rectangle 2ND using inputs supplied by the user...
+
+#include <stdio.h>
+
+int main()
+{
+   int length,breadth;
+   printf("What is the value of length of rectangle\n");
+   scanf("%d",&length);
+   printf("What is the value of breadth of rectangle\n");
+   scanf("%d",&breadth);
+   printf("The area of your rectangle is %d",length*breadth);
+
+    return 0;
+}
+
+
