72a3347897a7cb00c172c0b175ba177ea3385a58
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-11-26 23:00:03 +0530

    Add files via upload

---
+++ type declaration instructions in C.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main()
+{
+ //int a=4,b,c; //type declaration instructions types like--> Int,float,Character
+ // b = c = a;
+// printf("the value of a is %d \n",a);
+// printf("the value of b is %d \n",b);
+// printf("the value of c is %d \n",c);
+ 
+   // float ka Example
+   //float a=1.1;
+   //float b=a+8.9;
+  //printf("the value of b is%f \n",b);
+   
+   // some extra
+   int a,b,c,d;
+   a = b = c = d = 30;
+   printf("the value of a,b,c & d is %d each",a /*or b,c,d*/ );
+    return 0;
+}
+
+
+
